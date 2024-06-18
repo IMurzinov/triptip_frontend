400333b994ba043d376a3f7eff2c1348292d867d
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-06-18 17:03:10 +0300

    add profileCard css and jsx, Buttons folder, modified index.html, App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProfileCard from './components/Cards/ProfileCard/ProfileCard';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-      </Switch>
-    </Router>
+    // <Router>
+    //   <Switch>
+    //   </Switch>
+    // </Router>
+    <ProfileCard />
   );
 }
 
