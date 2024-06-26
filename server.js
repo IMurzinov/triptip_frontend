229f8cdd8b6cf785c11a19d10b251dd144df2f30
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-06-26 14:17:09 +0300

    added primaryButton component

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 8080;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on http://localhost:8080');
+});
+
+
+
+
+// const PORT = 8080;
+
+
+
+
+
+
 
 // Переадресация c http://localhost:8080/test
-app.get('/test', (req, res) => {
-  res.redirect('http://triptip.pro/test');
-});
-
-// Скачивает локальные файлы, если путь переадресации не был найден
-app.use(express.static('public'));
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}/`);
-});
+// app.get('/test', (req, res) => {
+//   res.redirect('http://triptip.pro/test');
+// });
