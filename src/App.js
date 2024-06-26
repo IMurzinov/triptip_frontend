ef1576c8a7594de75ac481036b76d1a1a88ec602
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-06-26 17:03:51 +0300

    added Header component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PrimaryButton from './components/Buttons/primary-button/PrimaryButton';
 import SecondaryButton from './components/Buttons/secondary-button/SecondaryButton';
 import DangerButton from './components/Buttons/danger-button/DangerButton';
+import Header from './components/Texts/Headers/Header';
 
 function App() {
   return (
@@ -9,6 +10,9 @@ function App() {
       <PrimaryButton className="primaryButton" text="Зарегистрироваться" onClick={() => { console.log('primaryButton click') }} />
       <SecondaryButton className="secondaryButton" text="Добавить город" onClick={() => { console.log('secondaryButton click') }} />
       <DangerButton className="dangerButton" text="Удалить аккаунт" onClick={() => { console.log('dangerButton click') }} />
+      <Header className="pageHeader" text="Регистрация" />
+      <Header className="sectionHeader" text="Общая информация" />
+      <Header className="formHeader" text="Город" />
     </div>
   );
 }
