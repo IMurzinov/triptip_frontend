eaa6d2332a0a1656959b0ebfb7ed609058efa5f1
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-10-11 02:00:21 +0300

    edited top trips card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { AuthPage, StartingPage, RegisterPage, NotFoundPage } from 'pages';
 
+import { TopTripsCard } from 'components';
+
 import 'assets/fonts/fonts.css';
 
 const router = createBrowserRouter([
