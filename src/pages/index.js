42da760ec02bd9eeef39d911badc5fc6888bedf5
Ivan Murzinov <murzinoff.ivan@gmail.com>
2024-12-05 17:16:08 +0300

    created welcome page for redirect after log in

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,3 +3,4 @@ export { default as AuthPage } from 'pages/AuthPage';
 export { default as RegisterPage } from 'pages/RegisterPage';
 export { default as NotFoundPage } from 'pages/NotFoundPage';
 export { default as ProfilePage } from 'pages/ProfilePage';
+export { default as WelcomePage } from 'pages/WelcomePage';
