d60b0d925624164a2fe44c570355e55f09475e66
Ivan Murzinov <murzinoff.ivan@gmail.com>
2025-02-14 21:03:31 +0300

    created basic tripCreate view

--- src/views/index.js
+++ src/views/index.js
@@ -3,3 +3,4 @@ export { default as AuthForm } from 'views/AuthForm';
 export { default as Toggler } from 'views/Toggler';
 export { default as TripList } from 'views/TripList';
 export { default as UserProfileCard } from 'views/UserProfileCard';
+export { default as TripCreate } from 'views/TripCreate';
