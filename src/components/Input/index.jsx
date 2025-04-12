76db3c485e9cd404bad40f63a4ea86d78457e993
Ivan Murzinov <murzinoff.ivan@gmail.com>
2025-04-12 21:11:51 +0300

    fixed problem with wrong autocomplete spell

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,23 +1,22 @@
 import React, { forwardRef } from "react";
-
 import "./index.css";
 
-const Input = forwardRef(({ label, type, placeholder, autoсomplete, ...props }, ref) => {
-    return (
-        <div className="form-input">
-            <label className="form-input__label">
-                {label}
-                <input
-                    ref={ref} // Пробрасываем ref прямо на <input />
-                    className="form-input__field"
-                    type={type}
-                    placeholder={placeholder}
-                    autoComplete={autocomplete}
-                    {...props} // Остальные пропсы, включая register
-                />
-            </label>
-        </div>
-    );
+const Input = forwardRef(({ label, type, placeholder, autoComplete, ...props }, ref) => {
+  return (
+    <div className="form-input">
+      <label className="form-input__label">
+        {label}
+        <input
+          ref={ref}
+          className="form-input__field"
+          type={type}
+          placeholder={placeholder}
+          autoComplete={autoComplete}
+          {...props}
+        />
+      </label>
+    </div>
+  );
 });
 
 export default Input;
